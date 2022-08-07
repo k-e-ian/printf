a9a899bdfbb925ab1f827e550f76bf6793ef9c43
Patricia Rukud'de <eri2pear@gmail.com>
2022-08-07 14:29:53 +0300

    Testing

--- main.h
+++ main.h
@@ -9,18 +9,4 @@ int _printf(const char *format, ...);
 
 int _putchar(char c);
 
-/**
- * buffer - a new type defining a buffer struct
- * @buffer - pointer to char array
- * @start - A pointer to the start of buffer
- * @length - length of the string stored in buffer.
- *
- */
-typedef struct
-{
-	char *buffer;
-	char *start;
-	unsigned int length;
-} buffer;
-
 #endif
