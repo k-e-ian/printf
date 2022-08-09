ac103b2815c73a72ab9bf045b970e55170bf4d7b
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-08-09 23:03:46 +0300

    track files

--- main.h
+++ main.h
@@ -1,26 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 
 int _putchar(char c);
 
-/**
- * buffer - a new type defining a buffer struct
- * @buffer - pointer to char array
- * @start - A pointer to the start of buffer
- * @length - length of the string stored in buffer.
- *
- */
-typedef struct
-{
-	char *buffer;
-	char *start;
-	unsigned int length;
-} buffer;
+int _puts(char *string);
+
+int _putd(int dec);
+
+int biC(unsigned int base10);
 
 #endif
