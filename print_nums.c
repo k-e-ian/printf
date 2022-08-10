15935e9d809be541e428dba6d50f40dd27bb77be
Ian Edwin Kitembe <ianedwin@outlook.com>
2022-08-10 05:30:00 +0300

    editing print base and print num functions

--- print_nums.c
+++ print_nums.c
@@ -12,9 +12,9 @@ int print_int(va_list l, flags_t *f)
 	int n = va_arg(l, int);
 	int res = count_digit(n);
 
-	if (f->space == 1 && f->plus == 0 && n >= 0)
+	if ((*f).space == 1 && f->plus == 0 && n >= 0)
 		res += _putchar(' ');
-	if (f->plus == 1 && n >= 0)
+	if ((*f).plus == 1 && n >= 0)
 		res += _putchar('+');
 	if (n <= 0)
 		res++;
